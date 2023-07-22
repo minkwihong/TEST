45503465e50ba0e0c3b985b63e30fc62dfc9a585
minkwihong <rnlghdals@naver.com>
2023-07-22 16:42:38 +0900

    통합

---
+++ src/Interior.java
@@ -0,0 +1,34 @@
+public class Interior {
+    private String tv;
+    private String sofa;
+    private String table;
+
+
+
+
+
+    public Interior(String tv , String sofa,String table){
+        this.tv = tv;
+        this.sofa = tv;
+        this.table = table;
+    }
+
+
+    public void makeGusil(String s , String v ){
+        System.out.println("s == > " + s);
+        System.out.println("v == > " + v);
+    }
+
+
+    public String makeToilet(){
+        return "";
+    }
+
+
+
+
+
+
+
+
+}
