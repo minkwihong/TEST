5c940c592087e1b26525ddd26da509f69d73f117
minkwihong <rnlghdals@naver.com>
2023-07-22 17:58:29 +0900

    새로운커밋4

--- src/TestMain.java
+++ src/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
             if(i % 10 == 0 && i > 0) System.out.println(i - 9 + " ~ "+ i + " = " + sum);
         }
 
-        System.out.println("안녕하세요!!!!!22233342222443333333333333443444444444555");
+        System.out.println("안녕하세요!!!!!2223334222244333333333333344344444444455522222");
 
     }
 }
